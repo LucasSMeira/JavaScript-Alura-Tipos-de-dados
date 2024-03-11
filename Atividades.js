8219fa774cbc38ba4fcbc59cab9eb858d49a6e1f
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-11 12:14:07 -0300

    Parte 1 + "return  + conversao para String"

---
+++ Atividades.js
@@ -0,0 +1,8 @@
+const texto = "As porcentagens possuem um erro de digitacao";
+const mudanca = texto.length;
+/*
+const num = 342523;
+const mud = String(num).length;
+console.log(mud);
+ */
+console.log(mudanca);
