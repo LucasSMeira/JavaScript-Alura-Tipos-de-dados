a2eaf9f00a6a0f1b414088612cf6c379f8407fd0
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-11 12:39:26 -0300

    Passo 2

--- Atividades.js
+++ Atividades.js
@@ -1,8 +1,14 @@
 const texto = "As porcentagens possuem um erro de digitacao";
 const mudanca = texto.length;
+const textoupper = texto.toLocaleUpperCase();
 /*
 const num = 342523;
-const mud = String(num).length;
+const mud = String(num).length; -> Convertemos para String e aí podemos usar os métodos .
 console.log(mud);
  */
-console.log(mudanca);
+console.log(mudanca +" "+ textoupper);
+
+let num;
+let variavel = null;
+console.log(num);
+console.log(variavel);
